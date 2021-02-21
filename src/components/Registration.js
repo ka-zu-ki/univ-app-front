e6295becee40c66fe6fd1c7306b851ef887e741a
Kazuki Yamashiro <yamakazuki0721@gmail.com>
2021-02-21 17:01:10 +0900

    Lessonのカラムを変更 #17

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -4,6 +4,40 @@ const Registration = () => {
   return (
     <>
       <h1>履修登録ページ</h1>
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>月</th>
+            <th>火</th>
+            <th>水</th>
+            <th>木</th>
+            <th>金</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>1</th>
+            <td>月１</td>
+            <td>火１</td>
+            <td>水１</td>
+            <td>木１</td>
+            <td>金１</td>
+          </tr>
+          <tr>
+          <th>2</th>
+          </tr>
+          <tr>
+          <th>3</th>
+          </tr>
+          <tr>
+          <th>4</th>
+          </tr>
+          <tr>
+          <th>5</th>
+          </tr>
+        </tbody>
+      </table>
     </>
   )
 }
